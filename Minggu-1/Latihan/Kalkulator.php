59451d3a7fd0ac91abcebf7b6200f6860a48cbd9
Ravanelly Haris Billyanto <ravanerri@gmail.com>
2022-03-16 16:11:00 +0700

    Create Kalkulator.php

---
+++ Minggu-1/Latihan/Kalkulator.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Kalkulator</title>
+</head>
+<body>
+<?php 
+	if(isset($_POST['hitung'])){
+		$bil1 = $_POST['bil1'];
+		$bil2 = $_POST['bil2'];
+		$operasi = $_POST['operasi'];
+		switch ($operasi) {
+			case 'tambah':
+				$hasil = $bil1+$bil2;
+			break;
+			case 'kurang':
+				$hasil = $bil1-$bil2;
+			break;
+			case 'kali':
+				$hasil = $bil1*$bil2;
+			break;
+			case 'bagi':
+				$hasil = $bil1/$bil2;
+			break;			
+		}
+	}
+	?>
+<form method="post" action="hello.php">			
+			bil 1    <input type="text" name="bil1" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Pertama"><br>
+			bil 2    <input type="text" name="bil2" class="bil" autocomplete="off" placeholder="Masukkan Bilangan Kedua">
+			<select class="opt" name="operasi">
+				<option value="tambah">+</option>
+				<option value="kurang">-</option>
+				<option value="kali">x</option>
+				<option value="bagi">/</option>
+			</select>
+			<input type="submit" name="hitung" value="Hitung" class="tombol">											
+		</form>
+		<?php if(isset($_POST['hitung'])){ ?>
+			hasil <input type="text" value="<?php echo $hasil; ?>" class="bil">
+		<?php }else{ ?>
+			<input type="text" value="0" class="bil">
+		<?php } ?>
+</body>
+</html>
