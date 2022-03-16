f0a2e316f6eb74979fd0c48e3f702156d3bea534
Ravanelly Haris Billyanto <ravanerri@gmail.com>
2022-03-16 15:10:52 +0700

    Create variabel.php

---
+++ Minggu-1/praktikum/variabel.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+  <title>Testing</title>
+ </head>
+ <body>
+ <?php 
+ $nim = "A11.2020.13074";
+ $nama = 'Ravanelly Haris Billyanto';
+ $matkul = 'Pemrograman Web Lanjut';
+echo "NIM : " . $nim . "<br>";
+echo "Nama :" . $nama . "<br>";
+echo "Mata Kuliah : $matkul"; ?> 
+ </body>
+</html>
