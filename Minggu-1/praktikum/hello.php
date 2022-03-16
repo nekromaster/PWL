b062d571cc380a721b3b08bcc9ac16f4b3066102
Ravanelly Haris Billyanto <ravanerri@gmail.com>
2022-03-16 15:02:13 +0700

    Update hello.php

--- Minggu-1/praktikum/hello.php
+++ Minggu-1/praktikum/hello.php
@@ -1,3 +1,8 @@
-<?php
-echo "Hello, Nama saya PHP ";
-?>
+<html>
+ <head>
+  <title>praktikum hello</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World! Nama Saya Ravanelly Haris</p>'; ?> 
+ </body>
+</html>
