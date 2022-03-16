4c91c392cfb94dbab1f9f687bd16ae90c5b92c0f
Ravanelly Haris Billyanto <ravanerri@gmail.com>
2022-03-16 15:14:39 +0700

    Create tipedata.php

---
+++ Minggu-1/praktikum/tipedata.php
@@ -0,0 +1,22 @@
+<html>
+ <head>
+  <title>Testing</title>
+ </head>
+ <body>
+ <?php
+$nim = "A11.2020.13074";
+$nama = 'Ravanelly Haris';
+$umur = 21;
+$nilai = 10;
+$status = TRUE;
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama<br>";
+print "Umur : " . $umur; print "<br>";
+printf ("Nilai : %.3f<br>", $nilai);
+if ($status)
+echo "Status : Aktif";
+else
+echo "Status : Tidak Aktif";
+?>
+ </body>
+</html>
