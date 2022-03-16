c0734e3b6940d6a142ab0139b92300bd9513e666
Ravanelly Haris Billyanto <ravanerri@gmail.com>
2022-03-16 16:49:16 +0700

    Create if-else.php

---
+++ Minggu-2/praktikum/if-else.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>if-else</title>
+</head>
+<body>
+nilai a: 2
+nilai b: 5
+<?php
+if ($mood=2 < $b=5) {
+echo "a lebih kecil daripada b";
+} elseif ($a == $b){
+echo "a bernilai setara dengan b";
+}else {
+echo "a lebih kecil daripada b";
+}
+?>
+</body>
+</html>
