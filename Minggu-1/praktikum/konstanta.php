e2851658154f48998a24c3c6a4c7e7a330de7f13
Ravanelly Haris Billyanto <ravanerri@gmail.com>
2022-03-16 15:17:35 +0700

    Create konstanta.php

---
+++ Minggu-1/praktikum/konstanta.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>Testing</title>
+ </head>
+ <body>
+ <?php
+   define ("NAMA", "Ravanelly Haris");
+   define ("NILAI", 10);
+   echo "Nama : " . NAMA;
+   echo "<br>Nilai : " . NILAI; ?> 
+ </body>
+</html>
