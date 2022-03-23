e26c5b2c0bc293f7ebfe7410ba7feb76e77e0114
Ravanelly Haris <ravanerri@gmail.com>
2022-03-23 18:50:51 +0700

    Create input01.php

---
+++ Minggu-3/Praktikum/input01.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Form</title></head>
+<body>
+<FORM ACTION="" METHOD="POST" NAME="input">
+Nama Anda : <input type="text" name="nama"><br>
+Email     : <input type="text" name="nama"><br>
+<input type="submit" name="Input" value="Input">
+</FORM>
+</body>
+</html>
+<?php
+if (isset($_POST['Input'])) { $nama = $_POST['nama'];
+echo "Nama Anda : <b>$nama</b>"; } ?>
