b52d70a9c4499663e7fa444c10039ef9058593cf
Ravanelly Haris <ravanerri@gmail.com>
2022-03-23 19:00:12 +0700

    Create inputcheckbox.php

---
+++ Minggu-3/Praktikum/inputcheckbox.php
@@ -0,0 +1,16 @@
+<html>
+<head><title>Band Favorit ~ Inputan Checkbox</title></head>
+<body>
+<FORM ACTION="prosescheckbox.php" METHOD="POST" NAME="input">
+<h2>Pilih Band Favorit Anda :</h2>
+<input type="checkbox" name="band01" value="slipknot" checked>
+Padi<br>
+<input type="checkbox" name="band02" value="Korn">
+Sheila On 7<br>
+<input type="checkbox" name="band03" value="Death Circus">Dewa
+19<br>
+<input type="checkbox" name="band04" value="My Chemical Romance">My Chemical Romance<br>
+<input type="submit" name="Pilih" value="Pilih">
+</FORM>
+</body>
+</html>
